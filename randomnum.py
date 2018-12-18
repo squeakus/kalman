cf0718d860c60b60f5f8ea3b87f173189269e71b
Jetal Mali <38008970+MaliJetal@users.noreply.github.com>
2018-12-18 19:34:53 +0530

    Add files via upload
    The code having good random values which give us the pretty recognition that noise is actually reducing about to zero.

---
+++ randomnum.py
@@ -0,0 +1,28 @@
+
+# Python code to generate 
+# random numbers and 
+# append them to a list 
+import random
+import pylab
+  
+# Function to generate 
+# and append them  
+# start = starting range, 
+# end = ending range 
+# num = number of  
+# elements needs to be appende
+
+  
+# Driver Code 
+num = 50
+start = 1
+end = 10
+measurements = [] 
+for j in range(num):
+    measurements.append(random.random()) 
+print(measurements)   
+
+#pylab.plot(Rand(start,end,num))
+
+    
+
